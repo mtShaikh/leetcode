9eb394b06710e6a9be930bd88aff4eedeccb21ca
taha <shaikh.taha95@gmail.com>
2018-12-03 18:35:35 +0500

    added asm file for insertion sort

---
+++ insertion-sort/c/insertion-sort.asm
@@ -0,0 +1,137 @@
+	.file	"insertion-sort.c"
+	.text
+	.globl	insertionSort
+	.type	insertionSort, @function
+insertionSort:
+.LFB5:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movl	$0, -12(%rbp)
+	jmp	.L2
+.L6:
+	movl	-12(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	movl	%eax, -4(%rbp)
+	movl	-12(%rbp), %eax
+	movl	%eax, -8(%rbp)
+	jmp	.L3
+.L5:
+	movl	-8(%rbp), %eax
+	cltq
+	salq	$2, %rax
+	leaq	-4(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	-8(%rbp), %edx
+	movslq	%edx, %rdx
+	leaq	0(,%rdx,4), %rcx
+	movq	-24(%rbp), %rdx
+	addq	%rcx, %rdx
+	movl	(%rax), %eax
+	movl	%eax, (%rdx)
+	subl	$1, -8(%rbp)
+.L3:
+	cmpl	$0, -8(%rbp)
+	jle	.L4
+	movl	-8(%rbp), %eax
+	cltq
+	salq	$2, %rax
+	leaq	-4(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	cmpl	%eax, -4(%rbp)
+	jl	.L5
+.L4:
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-4(%rbp), %eax
+	movl	%eax, (%rdx)
+	addl	$1, -12(%rbp)
+.L2:
+	movl	-12(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jl	.L6
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE5:
+	.size	insertionSort, .-insertionSort
+	.section	.rodata
+.LC0:
+	.string	"%d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB6:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$64, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$2, -48(%rbp)
+	movl	$5, -44(%rbp)
+	movl	$10, -40(%rbp)
+	movl	$3, -36(%rbp)
+	movl	$4, -32(%rbp)
+	movl	$1, -28(%rbp)
+	movl	$9, -24(%rbp)
+	movl	$6, -20(%rbp)
+	movl	$7, -16(%rbp)
+	movl	$8, -12(%rbp)
+	movl	$10, -52(%rbp)
+	movl	-52(%rbp), %edx
+	leaq	-48(%rbp), %rax
+	movl	%edx, %esi
+	movq	%rax, %rdi
+	call	insertionSort
+	movl	$0, -56(%rbp)
+	jmp	.L8
+.L9:
+	movl	-56(%rbp), %eax
+	cltq
+	movl	-48(%rbp,%rax,4), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	addl	$1, -56(%rbp)
+.L8:
+	movl	-56(%rbp), %eax
+	cmpl	-52(%rbp), %eax
+	jl	.L9
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L11
+	call	__stack_chk_fail@PLT
+.L11:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
+	.section	.note.GNU-stack,"",@progbits
