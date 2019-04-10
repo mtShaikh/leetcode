8d40283e9c4be5111bab4070bfa158adecf22214
taha <shaikh.taha95@gmail.com>
2019-04-10 13:19:06 +0500

    added 30 days of code

---
+++ hackerrank/30daysofcode/day1.go
@@ -0,0 +1,41 @@
+package main
+
+
+import (
+  "fmt"
+  "os"
+  "bufio"
+  "strconv"
+)
+
+func main() {
+  	var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
+
+    var i uint64 = 4
+    var d float64 = 4.0
+    var s string = "HackerRank "
+
+    scanner := bufio.NewScanner(os.Stdin)
+
+    // Declare second integer, double, and String variables.
+    var i2 int
+    var d2 float64
+    var s2 string
+
+    // Read and save an integer, double, and String to your variables.
+    scanner.Scan()
+    i2,_ = strconv.Atoi(scanner.Text())
+
+    scanner.Scan()
+    d2,_ = strconv.ParseFloat(scanner.Text(), 64)
+
+    scanner.Scan()
+    s2 = scanner.Text()
+    // Print the sum of both integer variables on a new line.
+    fmt.Println(uint64(i2) + i)
+    // Print the sum of the double variables on a new line.
+    fmt.Printf("%.1f\n",d2 + d)
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    fmt.Println(s + s2)
+}
